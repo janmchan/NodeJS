23c19fdc5fa7d325bc61ed4250e99b73d2d37e92
Jan Chan <jan.m.chan@hotmail.com>
2014-11-04 14:35:47 +0800

    fixed server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var port = process.env.PORT || 1337;
 
 //function start() {
   function onRequest(request, response) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
   }
 
   http.createServer(onRequest).listen(port);
