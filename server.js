f2b510fa84f71f31474cb537aa59b367c873c228
Jan Chan <jan.m.chan@hotmail.com>
2014-11-04 14:44:51 +0800

    Added default response

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ var port = process.env.PORT || 1337;
       {
             var uri = url.parse(request.url).pathname;
             var filename = path.join(process.cwd(), uri);
+            if(filename === '/')
+            {
+                 response.writeHead(200, {"Content-Type": "text/plain"});
+                    response.write("Welcome to Jan's Node JS Website");
+                    response.end();
+                    return;                
+            }
             fs.exists(filename, function(exists) {
                 if(!exists) {
                     response.writeHead(404, {"Content-Type": "text/plain"});
