5e5b8c7e60ff675d8a6df54ce2eedb980308fa02
Jan Chan <jan.m.chan@hotmail.com>
2014-11-04 14:38:33 +0800

    Added error handling

--- server.js
+++ server.js
@@ -8,8 +8,41 @@ var port = process.env.PORT || 1337;
 
 //function start() {
   function onRequest(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World\n');
+      try
+      {
+            var uri = url.parse(request.url).pathname;
+            var filename = path.join(process.cwd(), uri);
+            fs.exists(filename, function(exists) {
+                if(!exists) {
+                    response.writeHead(404, {"Content-Type": "text/plain"});
+                    response.write("404 Not Found\n");
+                    response.end();
+                    return;
+                }
+         
+                fs.readFile(filename, "binary", function(err, file) {
+                    if(err) {
+                        response.writeHead(500, {"Content-Type": "text/plain"});
+                        response.write(err + "\n");
+                        response.end();
+                        return;
+                    }
+             
+                    response.writeHead(200);
+                    response.write(file, "binary");
+                    response.end();
+                });
+            });
+      }
+      catch(ex)
+      {
+           response.writeHead(500, {"Content-Type": "text/plain"});
+                    response.write("Error has occured Not Found\n");
+                    response.write(ex.Message);
+                    response.end();
+                    return;
+      }
+   
   }
 
   http.createServer(onRequest).listen(port);
